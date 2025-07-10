6bfd9c44cb15c8134321b9a6f431f2619bd53cf5
Sergejs Boidenko <sergejs.boidenko@zabbix.com>
2025-07-10 11:09:54 +0300

    ........S. [ZBX-26591] fixed detection of read-only state in PostgreSQL replicated database

--- src/zabbix_proxy/proxy.c
+++ src/zabbix_proxy/proxy.c
@@ -1384,6 +1384,8 @@ static void	proxy_db_init(void)
 	char		*error = NULL;
 	int		db_type, version_check;
 
+	zbx_config_dbhigh->read_only_recoverable = 1;
+
 	if (SUCCEED != zbx_db_init(&error))
 	{
 		zabbix_log(LOG_LEVEL_CRIT, "cannot initialize database: %s", error);
